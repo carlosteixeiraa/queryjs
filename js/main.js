b6149a58db415cb2063f0282ed20cfe125b31768
carlosteixeiraa <carlosteixeira.email@gmail.com>
2017-10-15 17:57:25 +0100

    Upload ficheiros

---
+++ js/main.js
@@ -0,0 +1,40 @@
+﻿// Query JS
+var queryjs = (function(a) {
+    if (a == "") return {};
+    var b = {};
+    for (var i = 0; i < a.length; ++i)
+    {
+        var p=a[i].split('=', 2);
+        if (p.length == 1)
+            b[p[0]] = "";
+        else
+            b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+    }
+    return b;
+})(window.location.search.substr(1).split('&'));
+
+var nome = queryjs['nome']; 
+if (nome == "") {
+	nome = "QueryJS";
+}
+if (nome.length > 12) {
+	nome = "Muito Grande";
+}
+var valin;
+
+function vai() {
+	valin = document.getElementById("inpt").value
+	if (valin == "") {
+		valin = "QueryJS";
+	}
+}
+
+function bte() {
+	vai();
+	window.location.href = "index.html" + "?nome=" + valin
+}
+
+function mostrar() {
+	document.getElementById("nome").innerHTML = nome;
+}
+mostrar();
